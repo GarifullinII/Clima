aeee9bd993a418b91e746ef80855d7701a3304f6
Ildar <ildaridrisovich@gmail.com>
2023-02-13 02:37:56 +0400

    add stack view

--- Clima/Controllers/ViewController.swift
+++ Clima/Controllers/ViewController.swift
@@ -19,6 +19,46 @@ final class ViewController: UIViewController {
         return imageView
     }()
     
+    private let messagerButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "location.circle.fill"), for: .normal)
+        button.tintColor = .black
+        button.addTarget(self, action: #selector(messagerButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    private let searchTextField: UITextField = {
+        let textField = UITextField(frame: CGRect(x: 20, y: 0, width: 300, height: 40))
+        textField.placeholder = "Search"
+        textField.backgroundColor = .clear
+        textField.font = UIFont.systemFont(ofSize: 14)
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
+        textField.leftViewMode = .always
+        textField.layer.borderWidth = 1.0
+        textField.layer.borderColor = UIColor.black.cgColor
+        textField.layer.cornerRadius = 4
+        textField.textColor = .black
+        textField.clearButtonMode = .always
+        textField.returnKeyType = .done
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        return textField
+    }()
+    
+    private let searchButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+        button.tintColor = .black
+        button.addTarget(self, action: #selector(searchButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+
+        return button
+    }()
+    
+    var stackView = UIStackView()
     
     //MARK: - life cycle funcs
     override func viewDidLoad() {
@@ -29,8 +69,12 @@ final class ViewController: UIViewController {
     }
     
     //MARK: - flow funcs
-    private func setupViews() {
-        view.addSubview(backgroundImageView)
+    @objc private func messagerButtonTapped() {
+        print("messagerButtonTapped")
+    }
+    
+    @objc private func searchButtonTapped() {
+        print("messagerButtonTapped")
     }
     
     //MARK: - public
@@ -38,6 +82,30 @@ final class ViewController: UIViewController {
 
 //MARK: - extension
 extension ViewController {
+    private func setupViews() {
+        view.addSubview(backgroundImageView)
+        
+        stackView = UIStackView(
+            arrangedSubviews: [
+                messagerButton,
+                searchTextField,
+                searchButton
+                
+            ],
+            axis: .horizontal,
+            spacing: 5
+        )
+        
+        view.addSubview(stackView)
+    }
+}
+
+extension ViewController {
     private func setConstraints() {
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10)
+        ])
     }
 }
